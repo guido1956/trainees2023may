c1f6c41233d696f386975f70cb4fb6b4952b78d2
Guido22 <GuidoDulos@valori.nl>
2023-04-19 13:57:52 +0200

    Demo04 en 05 variabelen is nu demo4

--- src/main/java/valori/demos/Demo04Variables.java
+++ src/main/java/valori/demos/Demo04Variables.java
@@ -5,7 +5,24 @@ public class Demo04Variables {
         demoStrings();
         demoInts();
         demoDoubles();
-        demoBooleans();
+
+    }
+
+    public static void demoStrings() {
+        String myFirstName = "Walter";    // declaration and initialisation
+        String myLastName = "Scott"; //declaration and initialisation
+        String myfullName = myFirstName + " " + myLastName;
+        System.out.println(myFirstName);
+        System.out.println(myLastName);
+        System.out.println(myfullName);
+    }
+
+    public static void demoInts() {
+
+    }
+
+    public static void demoDoubles() {
+
     }
 
 
