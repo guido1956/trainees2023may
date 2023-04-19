a8df63d4138ff654c64857404df9250f695a442b
Guido22 <GuidoDulos@valori.nl>
2023-04-19 14:54:53 +0200

    Demo 7

---
+++ src/main/java/valori/demos/Demo07MethodsWithReturnType.java
@@ -0,0 +1,13 @@
+package valori.demos;
+import valori.output_console.*;
+
+public class Demo07MethodsWithReturnType {
+    public static void main(String[] args) {
+        String text = makeFullName("Jan", "Jansen");
+        OutputConsole.messageWithStars(text);
+    }
+
+    public static String makeFullName(String firstName, String lastName) {
+        return firstName + " " + lastname;
+    }
+}
---
+++ src/main/java/valori/output_console/OutputConsole.java
@@ -0,0 +1,9 @@
+package valori.output_console;
+
+public class OutputConsole {
+    public static void messageWithStars(String text) {
+        String stars = "*";
+        stars = stars.repeat(5);
+        System.out.println(stars + text + stars);
+    }
+}
