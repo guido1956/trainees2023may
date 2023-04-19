8d32c0e4f2a7032776a529d9fd37ce39ad544ab6
Guido22 <GuidoDulos@valori.nl>
2023-04-19 12:34:09 +0200

    Demo03 Hello Word

--- src/main/java/valori/demos/Demo03HelloWorld.java
+++ src/main/java/valori/demos/Demo03HelloWorld.java
@@ -10,3 +10,4 @@ public class Demo03HelloWorld {
     }
 }
 
+
---
+++ src/main/java/valori/demos/Demo04MethodsWithParameters.java
@@ -0,0 +1,21 @@
+package valori.demos;
+
+public class Demo04MethodsWithParameters {
+    public static void main(String[] args) {
+        showMessage("Hello World");
+        showMessageWithStars("Hello World");
+        showMessageWithTokens("Hello World", 10, "#");
+    }
+
+    public static void showMessage(String text) {
+        System.out.println(text);
+    }
+
+    public static void showMessageWithStars(String text) {
+        System.out.println("*".repeat(5) + text + "*".repeat(5));
+    }
+
+    public static void showMessageWithTokens(String text, int amount, String token) {
+        System.out.println(token.repeat(amount) + text + token.repeat(5));
+    }
+}
