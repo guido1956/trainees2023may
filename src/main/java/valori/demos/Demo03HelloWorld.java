3f64841e6537cff874b81c54a9ca6a35ecd63975
Guido22 <GuidoDulos@valori.nl>
2023-04-19 11:50:13 +0200

    Demo03 Hello Word

--- src/main/java/valori/demos/Demo03HelloWorld.java
+++ src/main/java/valori/demos/Demo03HelloWorld.java
@@ -2,6 +2,11 @@ package valori.demos;
 
 public class Demo03HelloWorld {
     public static void main(String[] args) {
+        message();
+    }
+
+    public static void message() {
+        System.out.println("Hello World");
     }
 }
 
